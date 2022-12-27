01fde7517508be74ba58cc124f6fa1ad429a2d89
Suzanne Banasihan <sjb04@ymail.com>
2022-12-27 15:06:05 -0800

    updated files

--- script.js
+++ script.js
@@ -1,390 +1,229 @@
-// variables
-const viewScore = document.getElementById('scoreboard')
-const startButton = document.querySelector('#start')
-const nextButton = document.querySelector('.next')
-const quizTimer = document.querySelector('#timer')
-const quizcontainerEl = document.querySelector('.quizcontainer')
-const questionText = document.querySelector('.Question')
-const answerEl = document.querySelector('.answers')
-const seeScore = document.querySelector('.finish')
-const questionContainerEl = document.querySelector('.questionContainer')
-const initialsInputEl = document.querySelector('.initialsInput')
-let submittedInital = document.getElementById('initial').value;
-const submitbtn = document.querySelector('.submit')
-const myForm = document.getElementById('myForm')
-const seeScoreboard = document.querySelector('.scoreboard')
-let showInitial = document.querySelector('.yourInitial')
-let showScore = document.querySelector('.yourScore')
+//Get Elements
+const startScreen = document.querySelector("#start");
+const startBtn = document.querySelector("#start-btn");
+const infoBox = document.querySelector(".info-box");
+const exitBtn = document.querySelector(".quit");
+const continueBtn = document.querySelector(".restart");
+const quizBox = document.querySelector(".quiz-box");
+const endBox = document.querySelector("#quiz-end");
+const submitBtn = document.querySelector("#save-score");
+const initialsText = document.querySelector("#initials");
+var existing = localStorage.getItem('results');
+existing = existing ? existing.split(',') : [];
+var queCount = 0;
+var counter = 60;
+var score = 0;
 
-let shuffledQuestions, currentQuestionIndex
-// function to start timer and and question, when click the start button 
+//If Continue Button Clicked
+continueBtn.onclick = ()=>{
+    infoBox.classList.add("hide");
+    startScreen.classList.remove("hide");
+};
 
+//If Start Button Clicked
+startBtn.onclick = () => {
+    function countdown(){
+        counter--;
+            if (counter === 0){
+                clearInterval(startCountdown)
+                quizEnd()
+            };
+    let timeRem = document.querySelector("#time-rem");
+    let timeTag = "<span>Time Left: "+ counter +"</span>"
+    timeRem.innerHTML = timeTag;
+    };
+    var startCountdown = setInterval(countdown, 1000);
+    startScreen.classList.add("hide");
+    quizBox.classList.remove("hide");
+    showQuestions(queCount)
+};
 
-// function to start timer
-
-startButton.addEventListener("click", gameTimer)
-
-let timer = 50;
-
-function gameTimer() {
-
-    let startTimer = setInterval(function () {
-        quizTimer.textContent = timer--;
-        if (timer === 0) {
-            // stop timer
-            clearInterval(startTimer)
-            // notify function to alert"Game Over"
-            endofQuestion()
-        }
-    }, 1000)
+//Get Questions and Options from Array
+function showQuestions(index){
+    if (queCount>=10){
+        return;
+    }
+    const queText = document.querySelector(".que-text");
+    const optionList = document.querySelector("#choices");
+    let queTag = "<span>"+ questions[index].numb + ". "+ questions[index].question +"</span>";
+    let optionTag = '<div class="option">'+ questions[index].options[0] + '<span></span></div>'
+                    + '<div class="option">'+ questions[index].options[1] + '<span></span></div>'
+                    + '<div class="option">'+ questions[index].options[2] + '<span></span></div>'
+                    + '<div class="option">'+ questions[index].options[3] + '<span></span></div>'
+    queText.innerHTML = queTag;
+    optionList.innerHTML = optionTag;
+    const option = optionList.querySelectorAll(".option");
+    for (let i = 0; i < option.length; i++) {
+        option[i].setAttribute("onclick", "optionSelected(this)");
+    }
 }
 
+//Show Next Question When Question is answered
+function optionSelected(answer){
+    if (queCount>=10){
+        return;
+    }
+    let userAns = answer.textContent;
+    let correctAns = questions[queCount].answer;
+    if(userAns == correctAns){
+        console.log("Answer is Correct");
+        const response = document.querySelector("#response");
+        response.innerHTML = '<div id="response"><span>Correct!</span></div>';
+        setTimeout(nextQuestion, 500)
+        score += 1
 
-
-// click start, then hide start button, show quizcontainer, setting next question
-
-startButton.addEventListener("click", startQuiz)
-
-function startQuiz() {
-    startButton.classList.add('hide')
-    quizcontainerEl.classList.remove('hide')
-    seeScore.classList.add('hide')
-    initialsInputEl.classList.add('hide')
-    // sort questions by random order
-
-    shuffledQuestions = qandas.sort(() => Math.random() - 0.5)
-
-    currentQuestionIndex = 0;
-    setNextQuestion();
-
-    console.log(shuffledQuestions)
+    }else{
+        console.log("Answer is Wrong");
+        const response = document.querySelector("#response");
+        response.innerHTML = '<div id="response"><span>Wrong!</span></div>';
+        setTimeout(nextQuestion, 500)
+        counter -= 5
+    }
 }
+function nextQuestion(){
+    queCount++;
+    if(queCount == 10){
+        
+        quizEnd()
+    };
+    showQuestions(queCount);
+    const response = document.querySelector("#response");
+    response.innerHTML = '<div id="response"><span></span></div>';
+    }
 
-
+//End Quiz if All Questions Completed or Timer Runs out
+function quizEnd(){
+    quizBox.classList.add("hide");
+    endBox.classList.remove("hide");
+    const scoreText = document.querySelector(".score");
+    let scoreTag = '<h3 class="score"> Your score was '+ score +' out of 10!</h3>';
+    scoreText.innerHTML = scoreTag; 
+}
+//Submit Initials
+submitBtn.onclick = () => {
+    let initials = initialsText.value;
+    //Store Initials and Score in Local Storage
+    var resultsDataObj = {
+        initials: initials,
+        score: score
+    }
+    localStorage.setItem((localStorage.length+1), JSON.stringify(resultsDataObj));
+    initialsText.value = ""
+    location.reload();
+}
 
 // questions and answers variables for quiz
 
-const quiz = [
+let questions = [
     {
+        numb: 1,
         question: "Javascript cannot change the value of a source (src).",
-        answers:
-            [{
-                text: "true",
-                correct: false
-            }, {
-                text: "false",
-                correct: true
-            },    
-
-            ]
-    }, {
+        answer: "false",
+        options: [
+            "true",
+            "false",
+            " ",
+            " ",
+        ]
+    },
+    {
+        numb: 2,
         question: "Webpages use CSS to style which properties?",
-        answers:
-            [{
-                text: "color",
-                correct: false
-            }, {
-                text: "font",
-                correct: false
-            }, {
-                text: "layout",
-                correct: false
-            }, {
-                text: "all of the above",
-                correct: true
-            },
-
-            ]
-    }, {
-        question: ,
-        answers:
-            [{
-                text: ";",
-                correct: false
-            }, {
-                text: "",
-                correct: true
-            }, {
-                text: "",
-                correct: false
-            }, {
-                text: "",
-                correct: false
-            },
-
-            ]
-    }, {
-        question: "",
-        answers:
-            [{
-                text: "",
-                correct: true
-            }, {
-                text: "",
-                correct: false
-            }, {
-                text: "",
-                correct: false
-            }, {
-                text: "",
-                correct: false
-            },
-
-            ]
-    }, {
-        question: "",
-        answers:
-            [{
-                text: "",
-                correct: true
-            }, {
-                text: "",
-                correct: false
-            }, {
-                text: "",
-                correct: false
-            }, {
-                text: "",
-                correct: false
-            },
+        answer: "all of the above",
+        options:[
+            "color",
+            "font",
+            "layout",
+            "all of the above",
         ]
-    }, {
-        question: ,
-        answers:
-            [{
-                text: ";",
-                correct: false
-            }, {
-                text: "",
-                correct: true
-            }, {
-                text: "",
-                correct: false
-            }, {
-                text: "",
-                correct: false
-            },
+    }, 
+    {
+        numb: 3,
+        question: "In HTML what does the alt attribute specify for an image?",
+        answer: "alternate text",
+        options: [
+            "altruistic concern",
+            "alternate text",
+            "altitudinous elevation",
+            "none of the above",
         ]
-    }, {
-        question: ,
-        answers:
-            [{
-                text: ";",
-                correct: false
-            }, {
-                text: "",
-                correct: true
-            }, {
-                text: "",
-                correct: false
-            }, {
-                text: "",
-                correct: false
-            },
+    }, 
+    {
+        numb: 4,
+        question: "Which of the following is not a CSS function?",
+        answer: "img().",
+        options: [
+            "attr().",
+            "calc().",
+            "img().",
+            "var().",
         ]
-    }, {
-        question: ,
-        answers:
-            [{
-                text: ";",
-                correct: false
-            }, {
-                text: "",
-                correct: true
-            }, {
-                text: "",
-                correct: false
-            }, {
-                text: "",
-                correct: false
-            },
+    }, 
+    {
+        numb: 5,
+        question: "In an HTML element the class name of an attribute is case sensitive.",
+        answer: "true",
+        options: [
+            "true",
+            "false",
+            " ",
+            " ",
         ]
-    }, {
-        question: ,
-        answers:
-            [{
-                text: ";",
-                correct: false
-            }, {
-                text: "",
-                correct: true
-            }, {
-                text: "",
-                correct: false
-            }, {
-                text: "",
-                correct: false
-            },
+    }, 
+    {
+        numb: 6,
+        question: "In an array index the first element is?",
+        answer: "0",
+        options: [
+            "1",
+            "AA",
+            "0",
+            "A",
         ]
-    }, {
-        question: ,
-        answers:
-            [{
-                text: ";",
-                correct: false
-            }, {
-                text: "",
-                correct: true
-            }, {
-                text: "",
-                correct: false
-            }, {
-                text: "",
-                correct: false
-            },
-
-
-            ]
-    }
+    }, 
+    {
+        numb: 7,
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Text Markup Language",
+            "Hyper Terminal Making Links",
+            "Highland Trade Markup List",
+            "none of the above",
+        ]
+    }, 
+    {
+        numb: 8,
+        question: "The most common way to add CSS is:",
+        answer: "external",
+        options: [
+            "external",
+            "internal",
+            "inline",
+            "incline",
+        ]
+    }, 
+    {
+        numb: 9,
+        question: "HTML elements include:",
+        answer: "all of the above",
+        options: [
+            "heading",
+            "paragraph",
+            "body",
+            "all of the above",
+        ]
+    }, 
+    {
+        numb: 10,
+        question: "In Javascript HTML elements can be hidden by changing the ___________ style.",
+        answer: "const",
+        options: [
+            "string",
+            "const",
+            "display",
+            "syntax",
+        ]
+    },
 
 ]
-
-
-// display new question.
-function setNextQuestion() {
-    resetQuestion()
-    showQuestion(shuffledQuestions[currentQuestionIndex])
-
-}
-
-
-
-function showQuestion() {
-
-    questionText.innerText = shuffledQuestions[currentQuestionIndex].question;
-
-    shuffledQuestions[currentQuestionIndex].answers.forEach(answer => {
-        let button = document.createElement('button')
-        button.innerText = answer.text
-        button.classList.add('answer')
-
-        if (answer.correct) {
-            button.dataset.correct = answer.correct;
-        }
-        button.addEventListener('click', selectAnswer)
-        answerEl.appendChild(button)
-    })
-
-}
-// delete and reset previous questions
-function resetQuestion() {
-    clearStatus(document.body)
-    nextButton.classList.add('hide')
-    while (answerEl.firstChild) {
-        answerEl.removeChild(answerEl.firstChild)
-    }
-}
-
-//check the answers and calculate scores
-answerEl.addEventListener("click", selectAnswer)
-
-function selectAnswer(e) {
-
-    var cnt;
-    let selectedAnswer = e.target;
-    let correct = selectedAnswer.dataset.correct;
-    console.log("selectedAnswer = " + selectedAnswer + "\n");
-    console.log("correct = " + correct + "\n");
-
-
-
-    setStatus(document.body, correct)
-    cnt = 0;
-    Array.from(answerEl.children).forEach(button => {
-        setStatus(button, button.dataset.correct)
-        console.log("foreach cnt = "+cnt+"\n")
-        cnt++;
-
-    })
-    if (shuffledQuestions.length > currentQuestionIndex + 1) {
-        nextButton.classList.remove('hide')
-    } else {
-        endofQuestion()
-    }
-}
-
-
-let score = 0;
-function setStatus(element, correct) {
-    console.log("Called setStatus" + "\n");
-    clearStatus(element)
-    console.log("setStaus>score = " + score + "\n");
-    console.log("correct = " + correct + "\n");
-
-
-    if (correct) {
-        score += 20;
-        element.classList.add('correct')
-    }
-    else {
-        timer--;
-        element.classList.add('wrong')
-    }
-}
-
-
-
-
-function clearStatus(element) {
-    element.classList.remove('correct')
-    element.classList.remove('wrong')
-}
-
-nextButton.addEventListener("click", () => {
-    currentQuestionIndex++
-    setNextQuestion()
-}
-)
-
-
-function endofQuestion() {
-    questionContainerEl.classList.add('hide')
-    seeScore.classList.remove('hide')
-    clearInterval(gameTimer)
-}
-
-seeScore.addEventListener("click", () => {
-    seeScore.classList.add('hide')
-    initialsInputEl.classList.remove('hide')
-    seeScoreboard.classList.add('hide')
-
-})
-
-myForm.addEventListener("submit", finalScore)
-
-function finalScore(e) {
-    e.preventDefault();
-    submittedInital = document.getElementById('initial').value;
-    localStorage.setItem("ID", submittedInital)
-    let myName = localStorage.getItem("ID");
-    console.log(submittedInital);
-    showInitial.innerHTML = myName;
-    showScore.textContent = score * 20;
-    seeScoreboard.classList.remove('hide')
-    myForm.classList.add('hide')
-}
-
-// viewScore.addEventListener('click', seeScores)
-
-// function seeScore() {
-//     let see = document.querySelector('.hide')
-//     let highScore = localStorage.getItem("ID")
-//     see.classList.remove("hide")
-//     see.innerText = highScore;
-
-// }
-
-
-
-
-
-// GIVEN I am taking a code quiz
-
-// WHEN I answer a question
-// THEN I am presented with another question
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-// WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
-// WHEN the game is over
-// THEN I can save my initials and my score
